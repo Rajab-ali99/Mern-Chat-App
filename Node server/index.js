43c9d6c6798405ae78d78f55694cef81ed275696
Rajab Ali <168520584+Rajab-ali99@users.noreply.github.com>
2025-02-04 22:29:34 +0500

    Update index.js

--- Node server/index.js
+++ Node server/index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 8080
 app.use(cookieParser())
 app.use(cors({
      origin: process.env.FRONTEND_URL ,
-     credentials: true
+     credentials: true,
+     "allowedHeaders": ["sessionId", "Content-Type"],
+    "exposedHeaders": ["sessionId"],
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false
 }))
 app.use(express.json())
 app.get('/',(request,response)=>{
